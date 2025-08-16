63a7995da3db92ea377e769207c4f2b521e3adf8
abbasgg <abbasgg@RUBIS15>
2025-08-16 11:38:48 +0400

    ggggg

--- PetShop/Models/ShopViewModel.cs
+++ PetShop/Models/ShopViewModel.cs
@@ -4,6 +4,10 @@ namespace PetShop.Models
 {
     public class ShopViewModel
     {
-        public List<Product>? Products { get; set; }
+        public List<Product> Products { get; set; } = new List<Product>();
+        public List<Category> Categories { get; set; } = new List<Category>();
+        public List<Tag> Tags { get; set; } = new List<Tag>();  
     }
+
 }
+
